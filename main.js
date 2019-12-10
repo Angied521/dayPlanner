14303d7bb9be8cbd4bb300595d2701becdec1326
Angie DePasquale <angied521@yahoo.com>
2019-12-10 17:50:36 -0500

    update

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 
+
+(function()
+{
+  // instantiate a moment object
+  var NowMoment = moment();
+  
+
+  // display value of moment object in #displayMoment div
+  var eDisplayMoment = document.getElementById('displayMoment');
+  eDisplayMoment.innerHTML = NowMoment;
+  
+})();
+
+// saving to local storage
+
+if (window.localStorage){
+    var txtcontent = document.getElementById('content');
+
+    txtcontent.value = localStorage.getItem('content');
+
+    txtcontent.addEventListener('input', function(){
+        localStorage.setItem('content',txtcontent.value);
+    }, false);
+}
+
+//change colors to grid
+
+function Myfunction () 
+{ 
+val = document.myform.text_area.value; 
+mydoc = document.open(); 
+mydoc.write(val); 
+mydoc.execCommand("saveAs",true,".html"); //you can change the .txt to your extention
+history.go(-1);
+}
+
+//change colors to grid
