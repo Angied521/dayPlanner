2b6add7834db9c8d0613dffc23e4107da29f97f1
Angie DePasquale <angied521@yahoo.com>
2019-12-13 09:25:32 -0500

    udates

--- main.js
+++ main.js
@@ -1,38 +1,60 @@
-
-
-(function()
-{
+(function () {
   // instantiate a moment object
   var NowMoment = moment();
-  
+
+  var loadInputValues = function () {
+    var contentVal = localStorage.getItem('content')
+    document.querySelector('#content').value = contentVal
+
+  }
 
   // display value of moment object in #displayMoment div
   var eDisplayMoment = document.getElementById('displayMoment');
   eDisplayMoment.innerHTML = NowMoment;
-  
+  loadInputValues()
 })();
+
 
 // saving to local storage
 
-if (window.localStorage){
-    var txtcontent = document.getElementById('content');
 
-    txtcontent.value = localStorage.getItem('content');
-
-    txtcontent.addEventListener('input', function(){
-        localStorage.setItem('content',txtcontent.value);
-    }, false);
+function myFunction(targetElementId) {
+  console.log(targetElementId)
+  var text = document.querySelector(`#${targetElementId}`).value
+  localStorage.setItem(`${targetElementId}`, text);
+  retrieve()
 }
+//retrieve saved local storage content
+//i would finish entering for all content id but this is not saving when refreshed
+
+function retrieve() {
+
+
+
+  var saved = localStorage.getItem(`${targetElementId}`, text);
+if (saved) {
+  text.innerHTML = saved
+}
+
+  //var text = localStorage.getItem("content1").value =saved;
+
+
+  // document.getElementById("content1").innerHTML = localStorage.targetElementId, text;
+
+  localStorage.getItem("content2");
+  console.log(localStorage.getItem("content2"));
+  document.getElementById("content2").innerHTML = localStorage.getItem("text");
+
+  
+}
+
+retrieve()
+
 
 //change colors to grid
 
-function Myfunction () 
-{ 
-val = document.myform.text_area.value; 
-mydoc = document.open(); 
-mydoc.write(val); 
-mydoc.execCommand("saveAs",true,".html"); //you can change the .txt to your extention
-history.go(-1);
-}
 
-//change colors to grid
+// id time to equal now moment
+
+
+
